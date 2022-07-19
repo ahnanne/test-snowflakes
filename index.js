23e4e9e421816967781946435ac6eb65e253843c
안예인 <54733637+ahnanne@users.noreply.github.com>
2022-07-19 23:55:31 +0900

    [A] 스타일링

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 import * as wasm from "@yein/snowflakes";
 
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const PARTICLES_ON_SCREEN = 245;
+canvas.width = document.documentElement.clientWidth;
+canvas.height = document.documentElement.clientHeight - 10;
+let w = canvas.width;
+let h = canvas.height;
+
+function clientResize() {
+  canvas.width = document.documentElement.clientWidth;
+  canvas.height = document.documentElement.clientHeight - 10;
+
+  w = canvas.width;
+  h = canvas.height;
+}
+
+window.addEventListener("resize", clientResize);
+
 const { SnowCanvas } = wasm;
-const test = SnowCanvas.new();
+const test = SnowCanvas.new(w, h, PARTICLES_ON_SCREEN);
+console.log(Object.getOwnPropertyNames(wasm.SnowCanvas));
 
 export default test;
